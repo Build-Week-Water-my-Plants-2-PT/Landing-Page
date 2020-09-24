2173a8967bb0b88bf0b13062f10c572a5843e75e
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2020-09-23 18:14:22 -0700

    Updated the landing page and contact us page

--- Contact_Us/contact_us.js
+++ Contact_Us/contact_us.js
@@ -7,3 +7,43 @@ function menuToggle() {
   var nav = document.getElementById("toggleIcon");
   nav.classList.toggle("active");
 }
+
+//Validating the Contact form
+
+function validate() {
+  const form = document.getElementById("form");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const emailTitle = document.getElementById("title");
+  const message = document.getElementById("message");
+  const error_message = document.getElementById("error_message");
+
+  error_message.style.padding = "10px";
+
+  var text;
+
+  if (name.value === "" || name.value === null) {
+    text = "Name is required";
+    error_message.innerHTML = text;
+    console.log(1);
+    return false;
+  }
+  if (email.value === "" || email.value === null || email.indexOf("@") == -1) {
+    text = "Please enter a valid email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (emailTitle.value === "" || emailTitle.value === null) {
+    text = "Title is required";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (message.value === "" || message.value === null) {
+    text = "What did you want to tell us?";
+    error_message.innerHTML = text;
+    return false;
+  }
+  text = "Form Submitted Successfully!";
+  error_message.innerHTML = text;
+  return true;
+}
